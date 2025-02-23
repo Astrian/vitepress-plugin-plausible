e75b8dd8242f119a0f2db11dd6cc9a4a0f71b70d
Astrian Zheng <happy.wheel6743@astrian.moe>
2025-02-23 19:35:51 +1100

    refactor: rename plugin and update for Plausible analytics integration

--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,25 @@ declare global {
 }
 }
 
-const mountGoogleAnalytics = (id: string) => {
+const mountPlausibleAnalytics = (domain: string, api: string) => {
   // avoid duplicated import
-  if (window.dataLayer && window.gtag) {
+  if (document.querySelector(`script[data-domain="${domain}"]`)) {
     return
   }
   
-  // insert gtag `<script>` tag
-  const gtagScript = document.createElement('script')
-  gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${id}`
-  gtagScript.async = true
-  document.head.appendChild(gtagScript)
-  // insert gtag snippet
-  window.dataLayer = window.dataLayer || []
-  // the gtag function must use `arguments` object to forward parameters
-  window.gtag = function () {
-    dataLayer.push(arguments)
-  }
-  gtag('js', new Date())
-  gtag('config', id)
+  // insert Plausible `<script>` tag
+  const plausibleScript = document.createElement('script')
+  plausibleScript.defer = true
+  plausibleScript.dataset.domain = domain
+  plausibleScript.dataset.api = api
+  plausibleScript.src = "https://plausible.io/js/script.js"
+  document.head.appendChild(plausibleScript)
 }
-/* global GA_ID, ga */
-export default ({id}) => {
-  // Google analytics integration
-  if (process.env.NODE_ENV === 'production' && id && typeof window !== 'undefined') {
-    mountGoogleAnalytics(id)
+
+/* global GA_ID, ga, PLAUSIBLE_DOMAIN, PLAUSIBLE_API */
+export default ({domain, api}) => {
+  // Plausible analytics integration
+  if (process.env.NODE_ENV === 'production' && domain && api && typeof window !== 'undefined') {
+    mountPlausibleAnalytics(domain, api)
   }
 }
